e9a833a4f7d9e74c9125c02d4c7290b5107e9265
Devon Peticolas <devon@chartbeat.com>
2017-02-22 22:48:38 -0500

    WIP

--- cpu.py
+++ cpu.py
@@ -1,50 +1,6 @@
 """
 http://pastraiser.com/cpu/gameboy/gameboy_opcodes.html
 """
-class Container():
-    def __repr__(self):
-        return "0x%x" % self.value
-
-
-class Register(Container):
-
-    def __init__(self):
-        self._value = 0x00
-
-    @property
-    def value(self):
-        return self._value
-
-    @value.setter
-    def value(self, byte):
-        self._value = byte
-
-
-class RegisterPair(Container):
-
-    def __init__(self, hi_reg, lo_reg):
-        self._hi = hi_reg
-        self._lo = lo_reg
-
-    @property
-    def value(self):
-        # do I need to mask these?
-        return (self._hi.value << 8) + self._lo.value
-
-    @value.setter
-    def value(self, word):
-        self._hi.value = (word & 0xFF00) >> 8
-        self._lo.value =  word & 0x00FF
-
-
-class ProgramCounter(Register):
-    pass
-
-
-class StackPointer(Register):
-    pass
-
-
 class CPU():
 
     CLOCK_SPEED = 0x400000
@@ -62,85 +18,61 @@ class CPU():
     def __repr__(self):
         """
         >>> CPU(A=0x12, H=0x45, clock=8)
-        CPU(A=0x12, H=0x45, clock=8)
+        CPU(A=0x12, H=0x45, clock=0x08)
         """
-        kvs = []
-        for k in ['A', 'F', 'B', 'C', 'D', 'E', 'H', 'L', 'PC', 'SP']:
-            v = getattr(self, k)
-            if v.value:
-                kvs.append('%s=%s' % (k, v))
-        if self.clock:
-            kvs.append('clock=%s' % self.clock)
-        return 'CPU(' + ', '.join(kvs) + ')'
+        kvs = sorted(self.registers.items(), key=lambda x: self.REGISTERS.index(x[0]))
+        kvs += [('PC', self.pc), ('SP', self.sp), ('clock', self.clock)]
+        kvs = filter(lambda x: x[1], kvs)
+        return 'CPU(' + ', '.join(map('%s=0x%02x'.__mod__, kvs)) + ')'
 
+    def __getitem__(self, key):
+        if key in self.REGISTERS:
+            return self.registers[key]
+        if key in self.REGISTER_PAIRS:
+            high_key, low_key = key
+            return (self.registers[high_key] << 8) + self.registers[low_key]
+        raise KeyError("%s not a register" % key)
 
-    def __getitem__(self, index):
-
-        return self.mem[index]
-
-    def __setitem__(self, index, value):
-        # don't allow writing to read only memory
-        if index < 0x8000:
-            return
-
-        # writing to ECHO (0xE000 - 0xFCFF) also writes in RAM (C000-DDFF)
-        elif 0xE000 <= index < 0xFE00:
-            self.mem[index] = value
-            self.mem[index - 0x2000] = value
-
-        # this area is restricted
-        elif 0xFEA0 <= index < 0xFEFF:
-            return
-
-        else:
-            self.mem[index] = value
-
+    def __setitem__(self, key, value):
+        if key in self.REGISTERS:
+            self.registers[key] = value
+            return None
+        if key in self.REGISTER_PAIRS:
+            high_key, low_key = key
+            self.registers[high_key] = (value & 0xFF00) >> 8
+            self.registers[low_key]  = value & 0x00FF
+            return None
+        raise KeyError("%s not a register" % key)
 
     def __init__(self, **kwargs):
 
-        # basic registers
-        self.A = Register()
-        self.B = Register()
-        self.C = Register()
-        self.D = Register()
-        self.E = Register()
-        self.F = Register()
-        self.H = Register()
-        self.L = Register()
-
-        # special registers
-        self.PC = ProgramCounter()
-        self.SP = StackPointer()
-
-        # register pairs (a 16-bit interface for two 8 bit registers)
-        self.AF = RegisterPair(self.A, self.F)
-        self.BC = RegisterPair(self.B, self.C)
-        self.DE = RegisterPair(self.D, self.E)
-        self.HL = RegisterPair(self.H, self.L)
+        self.pc = None
+        self.sp = None
 
         # always initialize the clock to 0
         self.clock = 0
 
+        # initalize registers to None
+        self.registers = {k: None for k in self.REGISTERS}
+
         # allow kwargs to initialize state
         self.set(**kwargs)
 
-    def set(self, clock=0, **container_kwargs):
+    def set(self, SP=None, PC=None, clock=None, **register_kwargs):
         if clock:
             self.clock = clock
-        for k, v in container_kwargs.items():
-            getattr(self, k).value = v
+        if SP:
+            self.sp = SP
+        if PC:
+            self.pc = PC
+        for k, v in register_kwargs.items():
+            self[k] = v
 
-    def set_initial_values(self):
-        self.PC.set(0x100)
-        self.SP.set(0xFFFE)
-        self.AF.set(0x01B1)
-        self.BC.set(0x0013)
-        self.DE.set(0x00D8)
-        self.HL.set(0x014D)
-
-    def execute_next_opcode(self):
-        pass
-
-    def update_timers(self):
-        pass
+    def set_startup_values(self):
+        self.pc    = 0x100
+        self.sp    = 0xFFFE
+        self['AF'] = 0x01B1
+        self['BC'] = 0x0013
+        self['DE'] = 0x00D8
+        self['HL'] = 0x014D
 
--- instructions.py
+++ instructions.py
@@ -7,47 +7,50 @@ def _ld_r8_d8(s, t):
 # ld A,B
 def _ld_r8_r8(cpu, t, s):
     """
+    >>> from cpu import CPU
     >>> cpu = CPU(A=0xaa, B=0xbb)
-    >>> _ld_r8_r8(cpu.A, cpu.B)()
+    >>> _ld_r8_r8(cpu, 'A', 'B')()
     >>> cpu
-    CPU(A=0xbb, B=0xbb, clock=4)
+    CPU(A=0xbb, B=0xbb, clock=0x04)
     """
     def _fn():
         cpu.clock += 4
-        t.value = s.value
+        cpu[t] = cpu[s]
     return _fn
 
 # ld A,(BC)
 def _ld_r8_deref_r16(cpu, mem, t, s):
     """
+    >>> from cpu import CPU
     >>> from memory import Memory
     >>> mem = Memory()
     >>> mem[0xbbcc] = 0xdd
     >>> cpu = CPU(A=0xaa, B=0xbb, C=0xcc)
-    >>> _ld_r8_deref_r16(mem, cpu.A, cpu.BC)()
+    >>> _ld_r8_deref_r16(cpu, mem, 'A', 'BC')()
     >>> cpu
-    CPU(A=0xdd, B=0xbb, C=0xcc, clock=8)
+    CPU(A=0xdd, B=0xbb, C=0xcc, clock=0x08)
     """
     def _fn():
         cpu.clock += 8
-        t.value = mem[s.value]
+        cpu[t] = mem[cpu[s]]
     return _fn
 
 # ld (BC),A
 def _ld_deref_r16_r8(cpu, mem, t, s):
     """
+    >>> from cpu import CPU
     >>> from memory import Memory
     >>> mem = Memory()
     >>> cpu = CPU(A=0xaa, B=0xbb, C=0xcc)
-    >>> _ld_deref_r16_r8(mem, cpu.BC, cpu.A)()
+    >>> _ld_deref_r16_r8(cpu, mem, 'BC', 'A')()
     >>> cpu
-    CPU(A=0xaa, B=0xbb, C=0xcc, clock=8)
+    CPU(A=0xaa, B=0xbb, C=0xcc, clock=0x08)
     >>> '0x%x' % mem[0xbbcc]
     '0xaa'
     """
     def _fn():
         cpu.clock += 8
-        mem[t.value] = s.value
+        mem[cpu[t]] = cpu[s]
     return _fn
 
 # ld A,(a16)
@@ -63,22 +66,29 @@ def _ld_deref_a16_r8(cpu, t, s):
 # ld BC,d16
 def _ld_r16_d16(cpu, t):
     """
+    >>> from cpu import CPU
     >>> cpu = CPU()
-    >>> _ld_r16_d16(cpu.BC)(0xffee)
+    >>> _ld_r16_d16(cpu, 'BC')(0xffee)
     >>> cpu
-    CPU(B=0xff, C=0xee, clock=12)
+    CPU(B=0xff, C=0xee, clock=0x0b)
     """
     def _fn(nn):
         cpu.clock += 12
-        t.value = nn
+        cpu[t] = nn
     return _fn
 
 def step_instruction(cpu, mem):
     return [
         # 0x00 NOP
-        _noop
+        _noop,
 
         # 0x01 LD BC,d16
+        _ld_r16_d16(cpu, 'BC'),
+
+        # 0x02 LD (BC),A
+        _ld_deref_r16_r8(cpu, 'BC', 'A'),
+
+        #
 
 
 
